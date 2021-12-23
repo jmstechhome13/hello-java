9473206fcc12434993aaeb15909c19a2214a194f
jmstechhome13 <jmstechhome13@gmail.com>
2021-12-23 04:47:32 +0000

    wishes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+  System.out.println("Happy cristmas ");
+    System.out.println("Happy new year ");
+    System.out.println("Happy sankranti ");
+
   }
 }
